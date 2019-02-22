a782b1950e7d80f8e13ae89353efbf39caec5561
negrimez <scorpion36rus@gmail.com>
2019-02-22 14:27:39 +0300

    LinkedList

---
+++ StructuresAndAlgorithmsVS/StructuresAndAlgorithms/LinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuresAndAlgorithms
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            this.data = data;
+        }
+        public T data { get; set; }
+        public Node<T> next { get; set; }
+    }
+    class LinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+                tail.next = node;
+            tail = node;
+            count++;
+        }
+        // удаление элемента
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        previous.next = current.next;
+                        if (current.next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        head = head.next;
+
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.next;
+            }
+            return false;
+        }
+
+    }
+}
